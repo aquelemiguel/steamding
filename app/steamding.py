a0d0a6f3f15178a611057a46fd8fc0f81923ac54
aquelemiguel <miguelmanofernandes@gmail.com>
2019-09-09 22:50:59 +0100

    Fix abspath not being a string causing exception

--- app/steamding.py
+++ app/steamding.py
@@ -21,7 +21,7 @@ gd_t, sa_t = None, None
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath('__file__')))
     return os.path.join(base_path, relative_path)
 
 def play_notification_sound(systrayicon = None):
