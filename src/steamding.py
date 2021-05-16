e777ab13ce295822508513295c244ab44412aa55
Miguel Fernandes <miguelmanofernandes@gmail.com>
2021-05-16 03:14:25 +0100

    Let appid thread open achievements thread

--- src/steamding.py
+++ src/steamding.py
@@ -5,27 +5,20 @@ from playsound import playsound
 from random import randrange
 import json
 from enum import Enum
-from threading import Lock, Thread
+from threading import Thread, Event
 
 cfg = configparser.ConfigParser()
-cfg.read('./src/cfg.ini')
+cfg.read('cfg.ini')
 
 class AchievementType(Enum):
     REGULAR = 1
     RARE = 2
     PLATINUM = 3
 
-appid = -1
-achieved_set = None
-rarity_set = {}
-
-lock = Lock()
-
-def update_appid(new_appid):
-    global appid
-    lock.acquire()
-    appid = new_appid
-    lock.release()
+def terminate_thread(thread, event):
+    if thread.is_alive():
+        event.clear()
+        thread.join()   
 
 def make_request(endpoint):
     try:
@@ -35,18 +28,21 @@ def make_request(endpoint):
     
 def fetch_achievement_percentages(appid):
     while True:
-        content = make_request(("https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/"
+        content = make_request(("https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/"
         f"?gameid={appid}?__random={randrange(10000000)}"))
 
         # This request must not fail, so retry if it doesn't succeed
         if content is None: continue
         return {ach['name']: ach['percent'] for ach in content['achievementpercentages']['achievements']}
 
-def fetch_playing_appid():
-    while True:
-        global appid, rarity_set
+def fetch_playing_appid(run_event):
+    appid = None
+    ach_t = Thread()
+    ach_run_event = Event()
+    ach_run_event.set()
 
-        content = make_request(("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/"
+    while run_event.is_set():
+        content = make_request(("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/"
         f"?key={cfg['AUTH']['apikey']}&steamids={cfg['AUTH']['steamid64']}?__random={randrange(10000000)}"))
 
         if content is None: continue
@@ -54,29 +50,32 @@ def fetch_playing_appid():
 
         # Started playing a new game, so we should update the appid and achievement percentages
         if 'gameid' in res and res['gameid'] != appid:
-            update_appid(res['gameid'])
+            appid = res['gameid']
             rarity_set = fetch_achievement_percentages(appid)
+            
+            terminate_thread(ach_t, ach_run_event)
+
+            ach_t = Thread(target=fetch_achievements, args=(appid, rarity_set, ach_run_event,))
+            ach_run_event.set()
+            ach_t.start()
 
         if 'gameid' not in res:
-            update_appid(-1)
+            appid = None
+            terminate_thread(ach_t, ach_run_event)
         
         time.sleep(int(cfg['GENERAL']['ping_delay_ms']) / 1000)
+    
+    terminate_thread(ach_t, ach_run_event)
 
-def fetch_achievements():
-    while True:
+def fetch_achievements(appid, rarity_set, run_event):
+    achieved_set = None
+
+    while run_event.is_set():
+        print(appid)
         time.sleep(0.5)
-        global achieved_set
 
-        lock.acquire()
-        if (appid == -1):
-            achieved_set = None
-            print('Waiting...')
-            lock.release()
-            continue
-
-        content = make_request(("https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/"
+        content = make_request(("https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/"
         f"?appid={appid}&key={cfg['AUTH']['apikey']}&steamid={cfg['AUTH']['steamid64']}?__random={randrange(10000000)}"))
-        lock.release()
 
         if content is None: continue
         new_achieved_set = {ach['apiname'] for ach in content['playerstats']['achievements'] if ach['achieved'] == 1}
@@ -94,10 +93,8 @@ def fetch_achievements():
 
                 if len(new_achieved_set) == len(rarity_set):
                     play_notification_sound(AchievementType.PLATINUM)
-                
                 elif rarity_set[new_ach] < float(cfg['GENERAL']['rare_percent']):
                     play_notification_sound(AchievementType.RARE)
-
                 else:
                     play_notification_sound(AchievementType.REGULAR)
             
@@ -105,17 +102,26 @@ def fetch_achievements():
 
 def play_notification_sound(type):
     playsound({
-        AchievementType.REGULAR: './assets/sfx/ps5/regular.mp3',
-        AchievementType.RARE: './assets/sfx/ps5-bliss/regular.mp3',
-        AchievementType.PLATINUM: './assets/sfx/ps5/platinum.mp3'
+        AchievementType.REGULAR: '../assets/sfx/ps5/regular.mp3',
+        AchievementType.RARE: '../assets/sfx/ps5-bliss/regular.mp3',
+        AchievementType.PLATINUM: '../assets/sfx/ps5/platinum.mp3'
     }[type])
 
-def start_tracking():
-    fpa_t = Thread(target=fetch_playing_appid)
-    ach_t = Thread(target=fetch_achievements)
+def start_threading():
+    fpa_run_event = Event()
+    fpa_run_event.set()
 
+    fpa_t = Thread(target=fetch_playing_appid, args=(fpa_run_event,))
     fpa_t.start()
-    ach_t.start()
+
+    try:
+        while True:
+            time.sleep(.1)
+    except KeyboardInterrupt:
+        print("Safely closing threads...")
+        fpa_run_event.clear()
+        fpa_t.join()
+        print("Goodbye!")
 
 if __name__ == "__main__":
-    start_tracking()
+    start_threading()
